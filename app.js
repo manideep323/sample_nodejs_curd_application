d52629e706fd7828ff982bd2e8e66668888ae70c
Manideep Gajavalli <manideepg@fortunapix.com>
2019-12-16 11:27:45 +0530

    developer flexible packages added

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 const port = 3002;
 // set up home route
 app.get('/', (request, respond) => {
-  respond.status(200).json({
-    message: 'Welcome to Project Support',
-  });
+    respond.status(200).json({
+        message: 'Welcome to Project Support',
+    });
 });
 app.listen(port, (request, respond) => {
-  console.log(`Our server is live on ${port}. Yay!`);
+    console.log(`Our server is live on ${port}. Yay!`);
 });
